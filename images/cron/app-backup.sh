2e1416313ff44cd8a84ecf19471a588d43790809
Ian Harrier <github@harrier.us>
2018-07-25 14:38:39 -0500

    Overhaul the backup, restore, and upgrade processes

--- images/cron/app-backup.sh
+++ images/cron/app-backup.sh
@@ -6,7 +6,7 @@ START_TIME=$(date +%s)
 
 cd "$HOST_PATH"
 
-if [ "$OPERATION" = "disable" ]; then
+if [ "$BACKUP_OPERATION" = "disable" ]; then
     echo "[W] Backups are disabled."
 else
     if [ ! -d backups ]; then
@@ -54,7 +54,7 @@ else
     rsync --archive --delete volumes/web/data/ backups/tmp_backup/home
 
     echo "[I] Backing up Bamboo database."
-    docker exec -i "$(docker-compose ps -q db)" su postgres -c 'pg_dump $POSTGRES_DB' > backups/tmp_backup/db.sql
+    PGPASSWORD=${POSTGRES_PASSWORD} pg_dump --host=db --username=${POSTGRES_USER} --dbname=${POSTGRES_DB} > backups/tmp_backup/db.sql
 
     echo "[I] Resuming Bamboo."
     STATE=$(curl --silent \
@@ -69,14 +69,14 @@ else
     else
         echo "[W] Failed to resume Bamboo."
     fi
-    
+
     echo "[I] Compressing backup."
     tar -zcf backups/$TIMESTAMP.tar.gz -C backups/tmp_backup .
 
     echo "[I] Removing working directory."
     rm -rf backups/tmp_backup
 
-    EXPIRED_BACKUPS=$(ls -1tr backups/*.tar.gz 2>/dev/null | head -n -$RETENTION)
+    EXPIRED_BACKUPS=$(ls -1tr backups/*.tar.gz 2>/dev/null | head -n -$BACKUP_RETENTION)
     if [ "$EXPIRED_BACKUPS" ]; then
         echo "[I] Cleaning up expired backup(s):"
         for BACKUP in $EXPIRED_BACKUPS; do
